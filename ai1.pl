e583a56c7aa5e282d9daf49b9e8c1a5f57d8950a
Laurent BILLON <laurent.billonc@gmail.com>
2013-10-07 13:23:02 +0200

    simplify ai1 module

--- ai1.pl
+++ ai1.pl
@@ -1,4 +1,4 @@
-:-module(ai1,[ask_move/4]).
+:-module(ai1,[ask_move_ai1/3]).
 
 ask_move_ai1(xMove,yMove,layObstacle).
 ask_move_ai1(1,1,true). % Lay obstacle at current position, then, move x+1 and y+1
