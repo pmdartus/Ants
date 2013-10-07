b226904713a92b545be0c2217054d132e149407e
Laurent Billon <laurent.billonc@gmail.com>
2013-10-07 11:49:34 +0200

    corrected typo

--- ai1.pl
+++ ai1.pl
@@ -1,3 +1,4 @@
 :-module(ai1,[ask_move/4]).
-ask_move_ai1(xMove,yMove,xPlace,yPlace).
-ask_move_ai1(1,1,2,2).
+
+ask_move_ai1(xMove,yMove,layObstacle).
+ask_move_ai1(1,1,true). % Lay obstacle at current position, then, move x+1 and y+1
--- main.pl
+++ main.pl
@@ -1,7 +1,7 @@
 :-  use_module(algo1).
 :- [initialGrid].
 
-ask_move_p1(xMove,yMove,xPlace,yPlace):-ask_move_algo1(xMove,yMove,xPlace,yPlace).
+ask_move_p1(xMove,yMove,xPlace,yPlace):-ask_move_ai1(xMove,yMove,xPlace,yPlace).
 
 
 
