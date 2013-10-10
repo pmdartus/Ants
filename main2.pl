a7a4f38c55a610273972024e32bbbdd470c6d354
Laurent Billon <laurent.billonc@gmail.com>
2013-10-10 12:43:33 +0200

    refactored datastructure

--- main2.pl
+++ main2.pl
@@ -2,6 +2,13 @@
 :-  use_module(ai2).
 :- [initialGrid].
 
+
+resources1([_,_,_,Resources1,_],Resources1).
+resources2([_,_,_,_,Resources2],Resources2).
+pos_p1([Player1_pos,_,_,_,_],Player1_pos).
+pos_p2([_,Player2_pos,_,_,_],Player2_pos).
+walls([_,_,Walls,_,_],Walls).
+
 % Using Board as the main data structure : 
 
 board_length(X):- X is 4.
@@ -13,7 +20,7 @@ board_length(X):- X is 4.
 %]
 
 
-ants(Board) :- game([[4,5,8,11,13,16],1,9],1).
+ants() :- game([1,9,[4,5,8,11,13,16],[],[]],1).
 
 %Get position of a player
 pos_player(Board,Player,Position) :- nth0(Player,Board,Position).
@@ -28,9 +35,9 @@ game(Board , _) :- win(2, Board),write('p2 win!'),nl.
 game(Board , 1) :- play1(Board , NewBoard), game(NewBoard , 2). 
 game(Board , 2) :- play2(Board , NewBoard), game(NewBoard , 1). 
 
-play1([B,P1,P2] , NewBoard):- 
-    NP1 is P1+1,
-    NewBoard = [B,NP1,P2],
+play1([Player1_pos|T], NewBoard):- 
+    NP1 is Player1_pos+1,
+    NewBoard = [NP1|T],
     write('p1 plays'),nl,
     write(NewBoard),
     get(_).
@@ -38,4 +45,5 @@ play1([B,P1,P2] , NewBoard):-
 
 play2(Board , NewBoard):- Board = NewBoard, write('p2 plays'),nl,write(NewBoard),get(_).
 
-win(Player,[B,P1,P2]) :- P1=3 .
+win(1,Board) :- pos_p1(Board,P),P=3 .
+win(2,Board) :- pos_p2(Board,P),P=3 .
