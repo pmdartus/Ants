525843210cd670bb2fb149072caad924b3d27ba6
lbillon <laurent.billonc@gmail.com>
2013-10-10 11:34:16 +0200

    Implemented board display

--- structure.pl
+++ structure.pl
@@ -1,12 +1,19 @@
 
 % Using Board as the main data structure : 
 
-ants(Board) :- game(
+board_length(X):- X is 4.
+
+ants(Board) :- print_board(
 [
-e,e,e,
-w,e,e,
-e,e,w
+e,e,e,w,
+w,e,e,w,
+e,e,w,e,
+w,e,e,w
 ],1).
+
+print_board([],P).
+print_board([T|Q],P) :- board_length(BL),length(Q,L),M is mod(L,BL),M==0, write(T),nl, print_board(Q,P).
+print_board([T|Q],P) :- write(T), print_board(Q,P).
 
 % And 2nd parameter represents player who is turn
 game(Board , _) :- win(1, Board),write('p1 win!'),nl.
