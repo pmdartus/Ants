575abc7779833f40affc1b2451baf72a451e0464
Laurent Billon <laurent.billonc@gmail.com>
2013-10-10 10:49:30 +0200

    added new structure

--- structure.pl
+++ structure.pl
@@ -1,19 +1,17 @@
 
 % Using Board as the main data structure : 
 
-
-
 ants(Board) :- game(Board,1).
 
 % And 2nd parameter represents player who is turn
-game(Board , _) :- win(1, Board),write('p1 win!').
-game(Board , _) :- win(2, Board),write('p2 win!').
+game(Board , _) :- win(1, Board),write('p1 win!'),nl.
+game(Board , _) :- win(2, Board),write('p2 win!'),nl.
 
 % Play and switch turns
 game(Board , 1) :- play1(Board , NewBoard), game(NewBoard , 2). 
 game(Board , 2) :- play2(Board , NewBoard), game(NewBoard , 1). 
 
-play1(Board , NewBoard):- Board = NewBoard.
-play2(Board , NewBoard):- Board = NewBoard.
+play1(Board , NewBoard):- Board = NewBoard, write('p1 plays'),nl.
+play2(Board , NewBoard):- Board = NewBoard, write('p2 plays'),nl.
 
-win(Player,Board) :- get_char(Y), Y=:=1.
+win(Player,Board) :- fail.
