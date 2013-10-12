1f255eb45872bb44230746ef0dd4669946122933
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2013-10-12 18:19:47 +0200

    Improve display

--- main.pl
+++ main.pl
@@ -4,12 +4,12 @@
 
 
 ants :- default_board(Board), write('Initial board is :     '),nl,display_board(Board, 1),game(Board,1).
-ants(Board) :-  write('Initial board is :     '),write(Board),nl,game(Board,1).
+ants(Board) :-  write('Initial board is :     '),nl,display_board(Board, 1),nl,game(Board,1).
 
 
 %Choose AI
-play1(Board , NewBoard) :- ai1:play(Board,NewBoard), write('p1 plays, board is now '),write(NewBoard),nl.
-play2(Board , NewBoard) :- ai2:play(Board,NewBoard), write('p2 plays, board is now '),write(NewBoard),nl.
+play1(Board , NewBoard) :- ai1:play(Board,NewBoard), write('p1 plays, board is now :'),nl,display_board(Board, 1),nl.
+play2(Board , NewBoard) :- ai2:play(Board,NewBoard), write('p2 plays, board is now :'),nl,display_board(Board, 1),nl.
 
 
 % Define win conditions
@@ -17,14 +17,14 @@ win(1,Board) :- pos_p1(Board,P),P=3 .
 win(2,Board) :- pos_p2(Board,P),P=3 .
 
 % Stop game if 1 player wins
-game(Board , _) :- win(1, Board),write('p1 win!'),nl.
-game(Board , _) :- win(2, Board),write('p2 win!'),nl.
+game(Board , _) :- win(1, Board),nl,display_board(Board, 1),write('p1 win!'),nl.
+game(Board , _) :- win(2, Board),nl,display_board(Board, 1),write('p2 win!'),nl.
 
 % Play and switch turns
 game(Board , 1) :- play1(Board , NewBoard), game(NewBoard , 2). 
 game(Board , 2) :- play2(Board , NewBoard), game(NewBoard , 1).
 
-display_board(_, 17) :- nl. 
+display_board(_, Index) :- board_length(Length), Index > Length * Length, nl. 
 display_board(Board, Index) :-  pos_p1(Board,Pos), 
                                 member(Index, [Pos]), 
                                 write('1'), 
@@ -40,5 +40,5 @@ display_board(Board, Index) :-  walls(Board,Pos),
 display_board(Board, Index) :-  write('_'), 
                                 NewIndex is Index+1, next_display(Index), display_board(Board , NewIndex).
 
-next_display(Index) :- Index mod 4 =:= 0, nl. 
+next_display(Index) :- board_length(Length), Index mod Length =:= 0, nl. 
 next_display(Index) :- write(' '). 
