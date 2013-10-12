c41e9747f50f451407d31751fb1facc0a2ec5ed3
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2013-10-12 18:09:29 +0200

    Add display simple

--- main.pl
+++ main.pl
@@ -3,12 +3,11 @@
 :- [board].
 
 
-ants :- default_board(Board), write('Initial board is :     '),write(Board),nl,game(Board,1).
+ants :- default_board(Board), write('Initial board is :     '),nl,display_board(Board, 1),game(Board,1).
 ants(Board) :-  write('Initial board is :     '),write(Board),nl,game(Board,1).
 
 
 %Choose AI
-
 play1(Board , NewBoard) :- ai1:play(Board,NewBoard), write('p1 plays, board is now '),write(NewBoard),nl.
 play2(Board , NewBoard) :- ai2:play(Board,NewBoard), write('p2 plays, board is now '),write(NewBoard),nl.
 
@@ -23,5 +22,23 @@ game(Board , _) :- win(2, Board),write('p2 win!'),nl.
 
 % Play and switch turns
 game(Board , 1) :- play1(Board , NewBoard), game(NewBoard , 2). 
-game(Board , 2) :- play2(Board , NewBoard), game(NewBoard , 1). 
+game(Board , 2) :- play2(Board , NewBoard), game(NewBoard , 1).
 
+display_board(_, 17) :- nl. 
+display_board(Board, Index) :-  pos_p1(Board,Pos), 
+                                member(Index, [Pos]), 
+                                write('1'), 
+                                NewIndex is Index+1, next_display(Index), display_board(Board , NewIndex).
+display_board(Board, Index) :-  pos_p2(Board,Pos), 
+                                member(Index, [Pos]), 
+                                write('2'), 
+                                NewIndex is Index+1, next_display(Index), display_board(Board , NewIndex).
+display_board(Board, Index) :-  walls(Board,Pos), 
+                                member(Index, Pos), 
+                                write('M'), 
+                                NewIndex is Index+1, next_display(Index), display_board(Board , NewIndex).
+display_board(Board, Index) :-  write('_'), 
+                                NewIndex is Index+1, next_display(Index), display_board(Board , NewIndex).
+
+next_display(Index) :- Index mod 4 =:= 0, nl. 
+next_display(Index) :- write(' '). 
