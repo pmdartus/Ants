e358313a96b75b88b8d4f1a8e7fa7f112e0ab604
Laurent Billon <laurent.billonc@gmail.com>
2013-10-15 16:21:20 +0200

    existeChemin

--- existeChemin.pl
+++ existeChemin.pl
@@ -3,7 +3,10 @@
 
 %%Ne fonctionne pas pour le moment, il boucle à l'infini, mais renvoie qqmm un chemin 
 
-test:- default_board(Board), existeChemin(Board,1,8,10,W,Chemin).
+%TODO : Inverser la récursion de la ligne 10
+% Essayé pendant 2 heures, j'en peux plus...
+
+test:- default_board(Board), existeChemin(Board,1,8,25,W,Chemin).
 
 existeChemin(_,_,A,A,W,Chemin):- write(1),W is 0, Chemin = A.
 existeChemin(Board,P,A,B,W,Chemin) :- write(2),players:get_move(Board, A, P, B), W is 1, Chemin = [B].
