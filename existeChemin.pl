2507cd7e3d18ada2ba06b062ea83eee9d3f58db2
lponton <louise.ponton@insa-lyon.fr>
2013-10-17 15:50:18 +0200

    Update existeChemin.pl
    chgt board

--- existeChemin.pl
+++ existeChemin.pl
@@ -6,7 +6,7 @@
 %TODO : Inverser la récursion de la ligne 10
 % Essayé pendant 2 heures, j'en peux plus...
 
-board([8,20,[1,2,3,4,5,6,7,11,12,13,14,17,18,19,22,24,25,26,29,30,31,32,33,34,35,36],[],[]]).
+board([8,20,[1,2,3,4,5,6,7,11,12,13,14,17,18,19,22,24,25,26,29,30,31,32,33,34,35,36],[]]).
 
 test:- board(Board), path(Board,8,1,25,Nodes).
 
