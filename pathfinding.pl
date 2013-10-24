fc9fdfb5d7231a740fe3ba1898b1f32ebc54e74d
cedric dupuis <c.dupuis@kreactive.com>
2013-10-24 18:41:24 +0200

    EPIC : added blazing fast way to test for a path.

--- pathfinding.pl
+++ pathfinding.pl
@@ -25,3 +25,15 @@ edge(Board,A,J,B) :- players:get_move(Board, A, J, B).
 
 path(Board,A,J,B,Nodes,Path,L) :- edge(Board,A,J,B),not(member(B,Nodes)), Path = [B],L is 1.
 path(Board,A,J,B,Nodes,Path,L) :- edge(Board,A,J,C),not(member(C,Nodes)),path(Board,C,J,B,[C|Nodes],PathCB,LCB),Path = [C|PathCB],L is LCB+1.
+
+
+spf(Board,A,J,B,Nodes,Path,L,Max) :- Max > 0,edge(Board,A,J,B),not(member(B,Nodes)), Path = [B],L is 1.
+spf(Board,A,J,B,Nodes,Path,L,Max) :- Max > 0,edge(Board,A,J,C),not(member(C,Nodes)),NMax is Max-1,spf(Board,C,J,B,[C|Nodes],PathCB,LCB,NMax),Path = [C|PathCB],L is LCB+1.
+
+start_accessible(Board,A,J) :- players:get_move(Board, A, J, C), assert(acc(C)).
+step_accessible(Board,J) :- acc(A),players:get_move(Board, A, J, C),not(acc(C)), assert(acc(C)),step_accessible(Board,J).
+clean_accessible :- retractall(acc(X)).
+
+accessible(Board,A,J,B) :- start_accessible(Board,A,J),step_accessible(Board,J).
+accessible(Board,A,J,B) :- acc(B),clean_accessible.
+accessible(Board,A,J,B) :- clean_accessible,fail.
