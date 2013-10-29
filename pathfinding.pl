5d845670efa799369464f570ec37f128d22f407e
cedric dupuis <c.dupuis@kreactive.com>
2013-10-29 01:23:43 +0100

    Created new methods to find shortest_path. Still didn't erase older ones because we aren't that sure of our choice

--- pathfinding.pl
+++ pathfinding.pl
@@ -22,6 +22,12 @@ path(Board,A,J,B,Path,Length) :- path(Board,A,J,B,[],Path,Length).
 % Example usage from main :  board:board(Board),shortest_path(Board,8,1,23,Path,Length).
 shortest_path(Board,A,J,B,Path,Length) :- findall([L],path(Board,A,J,B,[],_,L),Poids),min_list(Poids,Length), !, path(Board,A,J,B,[],Path,Length).
 
+shortest_p(Board,A,J,B,Nodes,Path,Length,Max) :- accessible(Board,A,J,B),spf(Board,A,J,B,Nodes,Path,Length,Max).
+shortest_p(Board,A,J,B,Nodes,Path,Length,Max) :- accessible(Board,A,J,B),not(spf(Board,A,J,B,Nodes,Path,Length,Max)),NMax is Max+1,shortest_p(Board,A,J,B,Nodes,Path,Length,NMax).
+
+
+first_shortest_p(Board,A,J,B,Nodes,Path,Length,Max) :- accessible(Board,A,J,B),spf(Board,A,J,B,Nodes,Path,Length,Max),!.
+first_shortest_p(Board,A,J,B,Nodes,Path,Length,Max) :- accessible(Board,A,J,B),not(spf(Board,A,J,B,Nodes,Path,Length,Max)),NMax is Max+1,first_shortest_p(Board,A,J,B,Nodes,Path,Length,NMax).
 
 % ----------------------------------------
 %           Private Methods
