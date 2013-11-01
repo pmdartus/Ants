93caa6815b0afb5abba7915cfe751cc1f9f7abb2
lponton <louise.ponton@insa-lyon.fr>
2013-11-01 21:04:53 +0100

    changement règles unique ressource
    not_resources est true s'il ne reste plus de resources dispo sur la map

--- resources.pl
+++ resources.pl
@@ -1,22 +1,37 @@
 :- module(ressources,[
 	carry_resource/2,
-	has_ressources/2
+	not_resources/3,
+	wintest/2,
+	init/2
 	]).
 
 % ----------------------------------------
 %           Public Methods
 % ----------------------------------------
 
+
+
+% Prise de la ressource
+
+
+
+
+% Voyage avec la ressource
 % carry_resource(+Board, +Player)
 % Return if the selected player is carrying a ressource
-carry_resource(Board, 1) :- pos_p1(Board, Pos), resources1(Board, Res), member(Pos, Res).
-carry_resource(Board, 2) :- pos_p2(Board, Pos), resources2(Board, Res), member(Pos, Res).
+carry_resource(Board, 1) :- pos_p1(Board, Pos), resources(Board, Res), member(Pos, Res).
+carry_resource(Board, 2) :- pos_p2(Board, Pos), resources(Board, Res), member(Pos, Res).
 
-% has_ressources(+Board, +Player)
-% Return if the selected user has ressources on the board
-has_ressources(Board, 1) :- resources1(Board, Res), length(Res, Length), Length =\= 0.
-has_ressources(Board, 2) :- resources2(Board, Res), length(Res, Length), Length =\= 0.
+% Pose de la ressource
 
+
+
+% Général ressource
+% board_ressources(+Board)
+% Return true if ressources are not anymore on the board (there're in the basis)
+not_resources(Board,B1,B2) :- board:resources(Board, Res), delete(Res,B1,T), delete(T,B2,[]).
+
+ 
 % ----------------------------------------
 %           Private Methods
 % ----------------------------------------
