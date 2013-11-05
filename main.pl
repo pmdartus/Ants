b6a835416342c5a3016bea86d8ed34c3e04d0c1c
Laurent Billon <laurent.billonc@gmail.com>
2013-11-05 16:33:27 +0100

    Add basic logging

--- main.pl
+++ main.pl
@@ -16,7 +16,7 @@ ants :- board:board(Board), write('Initial board is :     '),pos_p1(Board,J1),b_
 
 % ants(+Board).
 % Launch the game with a custom Board loaded from a file
-ants(Board, Size) :- load(Board, Size),ants.
+ants(Board, Size) :- load(Board, Size),log_board_name(Board) ,ants.
 
 
 % ----------------------------------------
@@ -27,9 +27,9 @@ ants(Board, Size) :- load(Board, Size),ants.
 % Is true when a player win
 
 % Official !!! Déjà Testé et CA MARCHE !
-win(1,Board) :- b_getval(b1,X), b_getval(b2,Y), resources:not_resources(Board,X,Y), board:resources(Board,Res), length(Res,LongR), delete(Res,Y,T), length(T,Long1), Long1>(LongR/2).
-win(2,Board) :- b_getval(b1,X), b_getval(b2,Y), resources:not_resources(Board,X,Y), board:resources(Board,Res), length(Res,LongR), delete(Res,X,T), length(T,Long2), Long2>(LongR/2).
-win(null,Board) :- b_getval(b1,X), b_getval(b2,Y), resources:not_resources(Board,X,Y), board:resources(Board,Res), length(Res,LongR), delete(Res,X,T), length(T,Long2), LongR =:= (Long2*2).
+win(1,Board) :- b_getval(b1,X), b_getval(b2,Y), resources:not_resources(Board,X,Y), board:resources(Board,Res), length(Res,LongR), delete(Res,Y,T), length(T,Long1), Long1>(LongR/2),Points1 is Long1,Points2 is LongR-Long1, log_points(Points1,Points2).
+win(2,Board) :- b_getval(b1,X), b_getval(b2,Y), resources:not_resources(Board,X,Y), board:resources(Board,Res), length(Res,LongR), delete(Res,X,T), length(T,Long2), Long2>(LongR/2),Points2 is Long2,Points1 is LongR-Long2, log_points(Points1,Points2).
+win(null,Board) :- b_getval(b1,X), b_getval(b2,Y), resources:not_resources(Board,X,Y), board:resources(Board,Res), length(Res,LongR), delete(Res,X,T), length(T,Long2), LongR =:= (Long2*2), log_points(0,0).
 % TODO : Add : win when there no longer exist a path between all remaining resources and a base
 % ie : the remaining resources are impossible to bring to a base
 
@@ -52,3 +52,8 @@ game(Board , 1) :- play1(Board , NewBoard), game(NewBoard , 2).
 game(Board , 2) :- play2(Board , NewBoard), game(NewBoard , 1).
 
 
+% Format of log files : 
+% 1 line per game
+% name of board, points for player 1, points for player 2
+log_board_name(Board) :- open('results.csv', append, Handle), write(Handle, Board),write(Handle, ','), close(Handle).
+log_points(Points1,Points2) :- open('results.csv', append, Handle), write(Handle, Points1),write(Handle, ','), write(Handle, Points2),write(Handle, '\n'), close(Handle).
