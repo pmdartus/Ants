c1735aa5b7e67b597ebfeffb5ffc850352b7487f
Laurent Billon <laurent.billonc@gmail.com>
2013-11-05 15:42:51 +0100

    Add logging code to resources.pl

---
+++ ai_spf.pl
@@ -0,0 +1,14 @@
+:- module(ai_basic,[play/4]).
+
+
+% ----------------------------------------
+%           Public Methods
+% ----------------------------------------
+
+% play(+Board, +Player, +Move, -NewBoard)
+% Return in new Board, the next position of the player
+play(Board, Player, Moves, NewBoard):-
+	length(Moves, Length),
+	Index is random(Length),
+	nth0(Index, Moves, NewPosition),
+    update_user_position(Board, Player, NewPosition, NewBoard).
