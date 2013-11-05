ed602a06bab81d5a090c8ee93cc28876718521ff
lponton <louise.ponton@insa-lyon.fr>
2013-11-05 18:24:36 +0100

    thales longueurchemin

--- ai_spf_near.pl
+++ ai_spf_near.pl
@@ -1,4 +1,4 @@
-% AI shortest path + resources near
+% AI shortest path + resources far away
 
 						
 						
@@ -18,18 +18,23 @@ play(Board, 1, Moves, NewBoard):-
 % return the resource farest from the player
 far_resource(Res,PJ1,LongRes,Index,1,Long,Pr) :-  nth0(Index,Res,PrNew),
 							% Calcul de la longueur
-						board:board_length(Size),PJ1/Size is Calcul1, PrNew/Size is Calcul2,
-						Calcul2-Calcul1 is LongNow, LongNow>=Long,	
+						board:board_length(Size),DivEnt1 is PJ1//Size,DivEnt2 is PrNew//Size,
+						Mod1 is PJ1 mod Size,Mod2 is PrNew mod Size,
+						thales(DivEnt1,DivEnt2,Mod1,Mod2,LongNow), LongNow>=Long,	
 							% Iteration
 						NewIndex is Index+1, NewIndex<LongRes, far_resource(Res,PJ1,LongRes,NewIndex,1,Long,Pr).
 
 far_resource(Res,PJ1,LongRes,Index,1,Long,Pr) :-  nth0(Index,Res,PrNew),
 							% Calcul de la longueur
-						board:board_length(Size),PJ1/Size is Calcul1, PrNew/Size is Calcul2,
-						Calcul2-Calcul1 is LongNow,LongNow<Long,	
+						board:board_length(Size),DivEnt1 is PJ1//Size,DivEnt2 is PrNew//Size,
+						Mod1 is PJ1 mod Size,Mod2 is PrNew mod Size,
+						thales(DivEnt1,DivEnt2,Mod1,Mod2,LongNow),LongNow<Long,	
 							% Iteration
 						NewIndex is Index+1, NewIndex<LongRes, far_resource(Res,PJ1,LongRes,NewIndex,1,LongNow,PrNew).
 
 
+thales(DivEnt1,DivEnt2,Mod1,Mod2,LongNow) :- LongNow1 is (DivEnt1-DivEnt2)*(DivEnt1-DivEnt2),
+									LongNow2 is (Mod1-Mod2)*(Mod1-Mod2),
+									LongNow is LongNow1+LongNow2.
 
 
