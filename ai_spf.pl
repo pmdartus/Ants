e6ce6c08a49e9eb3e94eec644b192357d1dbcd34
Kim CHOUARD <kim.chouard@gmail.com>
2013-11-05 19:06:27 +0100

    Walls now CAN'T be placed on Players, Bases and Ressources

--- ai_spf.pl
+++ ai_spf.pl
@@ -8,7 +8,7 @@
 % play(+Board, +Player, +Move, -NewBoard)
 % Return in new Board, the next position of the player
 
-
+% Search for a ressource
 play(Board, 1, Moves, NewBoard):-
     not(carry_resource(Board, 1)),
     pos_p1(Board,PJ1),b_getval(b1,B1),b_getval(b2,B2),
@@ -18,6 +18,7 @@ play(Board, 1, Moves, NewBoard):-
     write('I should move to '), write(PH),nl,
     update_user_position(Board, 1, PH, NewBoard).
 
+% Going back to base while carrying a ressource
 play(Board, 1, Moves, NewBoard):-
     carry_resource(Board, 1),
     pos_p1(Board,PJ1),b_getval(b1,B1),   
@@ -26,12 +27,14 @@ play(Board, 1, Moves, NewBoard):-
     write('I should move to '), write(PH),nl,
     update_user_position(Board, 1, PH, NewBoard).
 
+% Place a wall
 play(Board, 1, Moves, NewBoard):-
     pos_p2(Board,PJ2),b_getval(b2,B2),   
     pathfinding:appel(Board,PJ2,1,B2,[],[PH|PT],Leng,1),
     write('I should put a wall @ '),write(PH),nl,
     place_wall(Board, 1, PH, NewBoard).
 
+% Nothing to do
 play(Board,1,Moves,Board) :- write('I pass'),nl.
 
 
