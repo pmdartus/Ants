0c29ff474a8df582c57f3f79c44e883f5adc643b
Kim CHOUARD <kim.chouard@gmail.com>
2013-11-05 21:59:27 +0100

    New script python

--- runAnts.py
+++ runAnts.py
@@ -1,11 +1,14 @@
 import os,sys
-if(2==sys.argv.count):
-	  mapname=sys.argv[0]
-	  mapsize=sys.argv[1]
-	  call = 'ants('+mapname+', '+mapsize+')'
-else:
-		call = 'ants'
 
-for i in range(0,4):
-	cmd = '(swipl -s main.pl -g "'+call+', halt." > games/log_'+`i`+'.txt )&'
+if(3==len(sys.argv)):
+	  mapname=sys.argv[1]
+	  mapsize=sys.argv[2]
+	  call = "ants('"+mapname+"', "+mapsize+")"
+else:
+		call = "ants"
+
+print call
+
+for i in range(0,1):
+	cmd = 'swipl -s main.pl -g "'+call+', halt." > games/log_'+`i`+'.txt'
 	os.system(cmd)
