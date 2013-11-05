e9b5cf6a2e41b56b14c1260a28331ea5ae1eb582
seedy <seedy.dupuis@gmail.com>
2013-11-05 23:55:12 +0100

    Updated compare, still not working

--- ai_compare_paths.pl
+++ ai_compare_paths.pl
@@ -8,7 +8,7 @@ play(Board,1,Moves,NewBoard) :-
 									not(carry_resource(Board, 1)),
 									pos_p1(Board,P1),pos_p2(Board,P2),b_getval(b1,B1),b_getval(b2,B2),
     								board:resources(Board,Res),nth0(I,Res,Pr), Pr \=B1,Pr \=B2,
-    								is_my_path_shorter(Board,1,P1,P2,Pr,[PH|PT]),
+    								is_my_path_shorter(Board,1,P1,P2,Pr,[PH|PT],Path2),
     								write('Aim for resource '),write(Pr),nl,
     								write('I should move to '), write(PH),nl,
     								update_user_position(Board, 1, PH, NewBoard).
@@ -17,9 +17,10 @@ play(Board,1,Moves,NewBoard) :-
 									not(carry_resource(Board, 1)),
 									pos_p1(Board,P1),pos_p2(Board,P2),b_getval(b1,B1),b_getval(b2,B2),
     								board:resources(Board,Res),nth0(I,Res,Pr), Pr \=B1,Pr \=B2,
-    								not(is_my_path_shorter(Board,1,P1,P2,Pr,[PH|PT])),
-    								write('I should put a wall @ '),write(PH),nl,
-								    place_wall(Board, 1, PH, NewBoard).
+    								is_my_path_longer(Board,1,P1,P2,Pr,[PH|PT],Path2),
+    								nth0(I,Path2,W),write(W).
+    								write('I should put a wall at '),write(W),nl,
+								    place_wall(Board, 1, W, NewBoard).
 
 play(Board,1,Moves,NewBoard) :-     
 									carry_resource(Board, 1),
@@ -34,7 +35,7 @@ play(Board,2,Moves,NewBoard) :-
 									not(carry_resource(Board, 2)),
 									pos_p1(Board,P1),pos_p2(Board,P2),b_getval(b1,B1),b_getval(b2,B2),
     								board:resources(Board,Res),nth0(I,Res,Pr), Pr \=B1,Pr \=B2,
-    								is_my_path_shorter(Board,2,P1,P2,Pr,[PH|PT]),
+    								is_my_path_shorter(Board,2,P1,P2,Pr,Path1,[PH|PT]),
     								write('Aim for resource '),write(Pr),nl,
     								write('I should move to '), write(PH),nl,
     								update_user_position(Board, 2, PH, NewBoard).
@@ -43,9 +44,10 @@ play(Board,2,Moves,NewBoard) :-
 									not(carry_resource(Board, 2)),
 									pos_p1(Board,P1),pos_p2(Board,P2),b_getval(b1,B1),b_getval(b2,B2),
     								board:resources(Board,Res),nth0(I,Res,Pr), Pr \=B1,Pr \=B2,
-    								not(is_my_path_shorter(Board,2,P1,P2,Pr,[PH|PT])),
-    								write('I should put a wall @ '),write(PH),nl,
-								    place_wall(Board, 2, PH, NewBoard).
+    								is_my_path_longer(Board,2,P1,P2,Pr,Path1,[PH|PT]),
+                                    nth0(I,Path1,W),
+    								write('I should put a wall at '),write(W),nl,
+								    place_wall(Board, 2, W, NewBoard).
 
 play(Board,2,Moves,NewBoard) :-     
 									carry_resource(Board, 2),
@@ -61,6 +63,8 @@ play(Board,2,Moves,NewBoard) :-
 %           Private Methods
 % ----------------------------------------
 
-is_my_path_shorter(Board,2,P1,P2,Goal,Path1) :- pathfinding:appel(Board,P2,2,Goal,[],Path1,Length,1),pathfinding:appel(Board,P1,1,Goal,[],P,Leng,1),Length <= Leng,!, write('My path is shorter!'),write(Path1).
-is_my_path_shorter(Board,1,P1,P2,Goal,Path2) :- pathfinding:appel(Board,P1,1,Goal,[],Path2,Length,1),pathfinding:appel(Board,P2,2,Goal,[],P,Leng,1),Length < Leng,!, write('My path is shorter!'),write(Path2).
+is_my_path_shorter(Board,2,P1,P2,Goal,Path1,Path2) :- pathfinding:appel(Board,P2,2,Goal,[],Path2,Length,1),pathfinding:appel(Board,P1,1,Goal,[],P,Leng,1),Length < Leng,!, write('My path is shorter!'),write(Path2).
+is_my_path_shorter(Board,1,P1,P2,Goal,Path1,Path2) :- pathfinding:appel(Board,P1,1,Goal,[],Path1,Length,1),pathfinding:appel(Board,P2,2,Goal,[],P,Leng,1),Length < Leng,!, write('My path is shorter!'),write(Path1).
 
+is_my_path_longer(Board,2,P1,P2,Goal,Path1,Path2) :- pathfinding:appel(Board,P2,2,Goal,[],Path2,Length,1),pathfinding:appel(Board,P1,1,Goal,[],P,Leng,1),Length >= Leng,!, write('My path is longer!'),write(Path2).
+is_my_path_longer(Board,1,P1,P2,Goal,Path1,Path2) :- pathfinding:appel(Board,P1,1,Goal,[],Path1,Length,1),pathfinding:appel(Board,P2,2,Goal,[],P,Leng,1),Length >= Leng,!, write('My path is longer!'),write(Path1).
