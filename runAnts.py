d3533cbf42f03e71828363c373a3f4e49c4f876d
Laurent Billon <laurent.billonc@gmail.com>
2013-11-05 16:51:26 +0100

    improved runAnts

--- runAnts.py
+++ runAnts.py
@@ -1,5 +1,9 @@
-import os
-MAPNAME=''
+import os,sys
+if(1==sys.argv.count):
+    mapname=sys.argv[0]
+else:
+    mapname=''
+
 for i in range(0,10):
-	cmd = 'swipl -s main.pl -g "ants'+MAPNAME+' , halt." > games/log_'+`i`+'.txt '
+	cmd = 'swipl -s main.pl -g "ants'+mapname+' , halt." > games/log_'+`i`+'.txt '
 	os.system(cmd)
