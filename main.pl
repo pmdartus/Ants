d8bd2e5b78393682ca821604c7e0f7e15a5303ba
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2013-11-05 19:47:11 +0100

    Refractor of the winner state

--- main.pl
+++ main.pl
@@ -23,26 +23,37 @@ ants(Board, Size) :- load(Board, Size),log_board_name(Board) ,ants.
 %           Private Methods
 % ----------------------------------------
 
-% win(+Player, +Board).
-% Is true when a player win
-
-% Official !!! Déjà Testé et CA MARCHE !
-win(1,Board) :- b_getval(b1,X), b_getval(b2,Y), resources:not_resources(Board,X,Y), board:resources(Board,Res), length(Res,LongR), delete(Res,Y,T), length(T,Long1), Long1>(LongR/2),Points1 is Long1,Points2 is LongR-Long1, log_points(Points1,Points2).
-win(2,Board) :- b_getval(b1,X), b_getval(b2,Y), resources:not_resources(Board,X,Y), board:resources(Board,Res), length(Res,LongR), delete(Res,X,T), length(T,Long2), Long2>(LongR/2),Points2 is Long2,Points1 is LongR-Long2, log_points(Points1,Points2).
-% No more accessbile
-win(2,Board) :- 
+% blocked_state(+Board)
+% Check if the board is in a stuck state
+% No more resources
+blocked_state(Board) :- b_getval(b1,X), b_getval(b2,Y), resources:not_resources(Board,X,Y). 
+% Player 1 blocked
+blocked_state(Board) :- 
 	b_getval(b1,X),pos_p1(Board,P1),P1 \= X, pos_p2(Board,P2),P2 \= X, 
 	not(pathfinding:accessible(Board,P1,1,X)),!,write('Joueur 1 ne peut pas retourner à sa base'),nl.
-
-win(1,Board) :- 
+% Player 2 blocked
+blocked_state(Board) :- 
 	b_getval(b2,Y),pos_p2(Board,P2),P2 \= Y, pos_p1(Board,P1),P1 \= Y, 
 	not(pathfinding:accessible(Board,P2,2,Y)),!,write('Joueur 2 ne peut pas retourner a sa base'),nl.
 
-win(null,Board) :- b_getval(b1,X), b_getval(b2,Y), resources:not_resources(Board,X,Y), board:resources(Board,Res), length(Res,LongR), delete(Res,X,T), length(T,Long2), LongR =:= (Long2*2), log_points(0,0).
+% get_winner(+Board, -Winner)
+% return by the Winner the player which win
+get_winner(Board, 1) :- 
+	b_getval(b1,X), b_getval(b2,Y), board:resources(Board,Res), 
+	length(Res,LongR), delete(Res,Y,T), length(T,Long1), Long1>(LongR/2),Points1 is Long1,Points2 is LongR-Long1,
+	log_points(Points1,Points2).
+get_winner(Board, 2) :- 
+	b_getval(b1,X), b_getval(b2,Y), board:resources(Board,Res), 
+	length(Res,LongR), delete(Res,X,T), length(T,Long2), Long2>(LongR/2),Points2 is Long2,Points1 is LongR-Long2, 
+	log_points(Points1,Points2).
+get_winner(Board, null) :-
+	b_getval(b1,X), b_getval(b2,Y), board:resources(Board,Res), 
+	length(Res,LongR), delete(Res,X,T), length(T,Long2), LongR =:= (Long2*2), 
+	log_points(0,0).
 
-% TODO : Add : win when there no longer exist a path between all remaining resources and a base
-% ie : the remaining resources are impossible to bring to a base
-
+display_winner(1) :- write('/////// Player 1 win /////////').
+display_winner(2) :- write('/////// Player 2 win /////////').
+display_winner(null) :- write('/////// Match null /////////').
 
 % play1(+Board, ?NewBoard).
 % Make a turn for the user 1, it returns the new Board with a changes executed during this turn
@@ -55,9 +66,7 @@ play2(Board , NewBoard) :- available_moves(Board, 2, Moves), write('Moves availa
 
 % game(+Board, +Player).
 % Change the player turn and verify first if a player won the game during the last turn
-game(Board , _) :- win(1, Board),write('//////  P1 win! /////////'), display_board(Board).
-game(Board , _) :- win(2, Board),write('//////  P2 win! /////////'), display_board(Board).
-game(Board , _) :- win(null, Board),write('//////  Match null /////////'), display_board(Board).
+game(Board , _) :- blocked_state(Board), get_winner(Board, X), display_winner(X), display_board(Board).
 game(Board , 1) :- play1(Board , NewBoard), game(NewBoard , 2). 
 game(Board , 2) :- play2(Board , NewBoard), game(NewBoard , 1).
 
